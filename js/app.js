60d5aadfce6dedebb0fa14631d7897a447327c11
juancarlucci <juancarloscollins@gmail.com>
2018-02-08 11:27:52 -0800

    fix flex items ovelapping footer

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
-  var project=$('#projects').offset();
-  console.log(project);
-  $('a').on('click',function(e){
-    e.preventDefault();
-    // console.log(project.left);
-    // $('h1').scrollLeft(project.left);
-
-    $('#title').animate({
-    width: "70%",
-    opacity: 0.4,
-    offset.left: "0.6in",
-    fontSize: "3.6em",
-    borderWidth: "10px"
-  }, 1500 );
-
-});//emd click
+//   var project=$('#projects').offset();
+//   console.log(project);
+//   $('a').on('click',function(e){
+//     e.preventDefault();
+//     // console.log(project.left);
+//     // $('h1').scrollLeft(project.left);
+//
+//     $('#title').animate({
+//     width: "70%",
+//     opacity: 0.4,
+//     fontSize: "3.6em",
+//     borderWidth: "10px"
+//   }, 1500 );
+//
+// });//emd click
 });
